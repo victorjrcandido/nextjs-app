d4c7b64e50006de2d78d318c159ec0b4235a8c1d
Victor Candido <victorjrcandido@gmail.com>
2023-08-14 16:08:02 -0300

    footer import fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import Navbar from '@/components/navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Footer from '@/components/footer/footer'
+import Navbar from '@/components/navbar/Navbar'
+import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
